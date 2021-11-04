155f34cd88f26e193ef70d92e62e7b27020e6eb7
ipCoelho <ipcoelho.personal@gmail.com>
2021-11-04 09:41:14 -0300

    feat: favicon added.

--- produtos/index.php
+++ produtos/index.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles-global.css" />
     <link rel="stylesheet" href="./produtos.css" />
+    <link rel="shortcut icon" href="../img/logo.png" type="image/x-icon">
     <title>Administrar Produtos</title>
 </head>
 <body>
